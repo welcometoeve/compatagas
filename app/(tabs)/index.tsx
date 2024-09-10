33c695951b0f0345f12e8cbf8ad248babae6109a
maayanalbert <maayan.albert@gmail.com>
2024-09-10 11:09:44 -0400

    position from top

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react"
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Switch,
+  SafeAreaView,
+} from "react-native"
 
 type Question = {
   label: string
@@ -10,7 +17,7 @@ type Person = {
   name: string
 }
 
-const questions: Question[] = [
+const basicQuestions: Question[] = [
   {
     label: "What's their favorite season?",
     options: ["Spring 🌸", "Summer ☀️", "Fall 🍂", "Winter ❄️"],
@@ -34,6 +41,41 @@ const questions: Question[] = [
   },
 ]
 
+const compatagramQuestions: Question[] = [
+  {
+    label: "Perpetually horny or nun or perpetually horny nun?",
+    options: ["Nun 🙏", "Perpetually horny 😈", "Perpetually horny nun 😇😈"],
+  },
+  {
+    label: "Eat a sandwich or get walked around on a leash like a dog?",
+    options: ["Eat a sandwich 🥪", "Get walked on a leash 🐕"],
+  },
+  {
+    label: "From the front or from the back?",
+    options: ["From the front 😊", "From the back 😏"],
+  },
+  {
+    label: "Church or dropping it like a thotty?",
+    options: ["Church 🙏", "Dropping it like a thotty 💃"],
+  },
+  {
+    label: "Classified camera roll or open book?",
+    options: ["Open book 📖", "Classified camera roll 🔒"],
+  },
+  {
+    label: "Sex on the first date or leave room for Jesus?",
+    options: ["Leave room for Jesus 🙏", "Sex on the first date 😘"],
+  },
+  {
+    label: "Do whips and chains excite you?",
+    options: ["No 🚫", "Yes 🔗"],
+  },
+  {
+    label: "Sending selfies or sending feet pics?",
+    options: ["Sending selfies 🤳", "Sending feet pics 🦶"],
+  },
+]
+
 const people: Person[] = [
   { name: "Samira" },
   { name: "Maarek" },
@@ -44,39 +86,79 @@ const people: Person[] = [
 export default function App() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
   const [currentPersonIndex, setCurrentPersonIndex] = useState(0)
+  const [isCompatagram, setIsCompatagram] = useState(false)
 
   const handleAnswer = () => {
+    const questions = isCompatagram ? compatagramQuestions : basicQuestions
     setCurrentQuestionIndex((prevIndex) => (prevIndex + 1) % questions.length)
     setCurrentPersonIndex((prevIndex) => (prevIndex + 1) % people.length)
   }
 
+  const toggleQuestionType = () => {
+    setIsCompatagram((prev) => !prev)
+    setCurrentQuestionIndex(0)
+  }
+
+  const questions = isCompatagram ? compatagramQuestions : basicQuestions
   const currentQuestion = questions[currentQuestionIndex]
   const currentPerson = people[currentPersonIndex]
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.name}>{currentPerson.name}</Text>
-      <Text style={styles.question}>{currentQuestion.label}</Text>
-      {currentQuestion.options.map((option, index) => (
-        <TouchableOpacity
-          key={index}
-          style={styles.button}
-          onPress={handleAnswer}
-        >
-          <Text style={styles.buttonText}>{option}</Text>
-        </TouchableOpacity>
-      ))}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text style={styles.name}>{currentPerson.name}</Text>
+        <Text style={styles.question}>{currentQuestion.label}</Text>
+        {currentQuestion.options.map((option, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.button}
+            onPress={handleAnswer}
+          >
+            <Text style={styles.buttonText}>{option}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <View style={styles.toggleContainer}>
+        <Text style={styles.toggleText}>Basic</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isCompatagram ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleQuestionType}
+          value={isCompatagram}
+        />
+        <Text style={styles.toggleText}>Compatagram</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#121212", // Dark background
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    padding: 20,
+    paddingTop: "45%",
+  },
+  toggleContainer: {
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
-    backgroundColor: "#121212", // Dark background
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#1E1E1E", // Slightly lighter than the background for contrast
+  },
+  toggleText: {
+    color: "#FFFFFF",
+    marginHorizontal: 10,
   },
   name: {
     fontSize: 24,
